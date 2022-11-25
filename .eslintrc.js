bc9e6cd0072468f38edbbd01e8e9504b21524823
yjkim0109 <yjkkim0109@gmail.com>
2022-11-25 22:03:44 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+	overrides: [
+		{
+			files: ['src/**/*.{ts,tsx}'],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: ['./tsconfig.json'],
+			},
+			rules: {
+				'preferred-import/ts-imports': 'error',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'preferred-import'],
+	rules: {},
+};

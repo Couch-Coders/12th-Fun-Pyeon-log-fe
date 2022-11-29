889542303452c368ea6ec655ead4e9f7fba3ee16
michoball <myunghun0114@gmail.com>
2022-11-29 23:15:54 +0900

    feat : Add all dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-	overrides: [
-		{
-			files: ['src/**/*.{ts,tsx}'],
-			parser: '@typescript-eslint/parser',
-			parserOptions: {
-				project: ['./tsconfig.json'],
-			},
-			rules: {
-				'preferred-import/ts-imports': 'error',
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react', 'preferred-import'],
-	rules: {},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  overrides: [
+    {
+      files: ["src/**/*.{ts,tsx}"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      rules: {
+        "preferred-import/ts-imports": "error",
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "preferred-import"],
+  rules: {
+    "@typescript-eslint/triple-slash-reference": { types: "always" },
+  },
 };

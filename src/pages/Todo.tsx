46c28726f6e699c46ca60e2b744a156a851681d3
michoball <myunghun0114@gmail.com>
2022-11-30 00:43:34 +0900

    feat : Add Todo page

---
+++ src/pages/Todo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ArrowDownOutlined } from "@ant-design/icons";
+
+export const Todo = () => {
+  return (
+    <div style={{ fontSize: "30px" }}>
+      Todo page <ArrowDownOutlined />
+    </div>
+  );
+};

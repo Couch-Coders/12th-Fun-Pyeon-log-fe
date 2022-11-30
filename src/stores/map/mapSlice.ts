226f4657a4c8ad95115f55f0ff6f8e22e6996cc4
yjkim0109 <yjkkim0109@gmail.com>
2022-11-30 22:08:06 +0900

    refactor: Edit MapData interface

--- src/stores/map/mapSlice.ts
+++ src/stores/map/mapSlice.ts
@@ -10,20 +10,16 @@ const getMapThunk = createAsyncThunk('mapSlice/getMap', async () => {
 		},
 	};
 	const resp = await axios(config);
-	console.log(resp);
 	return resp.data.documents;
 });
 
 export interface MapData {
 	address_name: string;
-	category_group_code: string;
-	category_group_name: string;
 	category_name: string;
 	distance?: string;
 	id: string;
 	phone?: string;
 	place_name: string;
-	place_url: string;
 	road_address_name: string;
 	x: string;
 	y: string;

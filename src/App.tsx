b1aa87c2a94f0048584250b767c4f760ac7df3d0
yjkim0109 <yjkkim0109@gmail.com>
2022-11-30 21:14:35 +0900

    feat: Add map page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import { Todo } from "@pages/Todo";
+import { Todo } from '@pages/Todo';
+import Map from '@pages/Map';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/todo" element={<Todo />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/todo' element={<Todo />} />
+				<Route path='/map' element={<Map />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;

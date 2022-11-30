28a8fe2c05c1242b97c5bc40f2c2c729d32ef00a
yjkim0109 <yjkkim0109@gmail.com>
2022-11-30 21:14:55 +0900

    chore: Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
-  overrides: [
-    {
-      files: ["src/**/*.{ts,tsx}"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-      rules: {
-        "preferred-import/ts-imports": "error",
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "preferred-import"],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/consistent-type-definitions": "warn",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    // "@typescript-eslint/no-floating-promises": "off",
-    "@typescript-eslint/no-misused-promises": "off",
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+	overrides: [
+		{
+			files: ['src/**/*.{ts,tsx}'],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: ['./tsconfig.json'],
+			},
+			rules: {
+				'preferred-import/ts-imports': 'error',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'preferred-import'],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/consistent-type-definitions': 'warn',
+		'@typescript-eslint/strict-boolean-expressions': 'off',
+		'@typescript-eslint/no-floating-promises': 'off',
+		'@typescript-eslint/no-misused-promises': 'off',
+	},
 };

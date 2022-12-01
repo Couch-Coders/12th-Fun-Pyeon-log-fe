dcd2742d8be3e1ec956f4440c6df3854ccab16e5
yjkim0109 <yjkkim0109@gmail.com>
2022-12-01 17:39:08 +0900

    feat: Change search keyword of map thunk

--- src/stores/map/mapType.ts
+++ src/stores/map/mapType.ts
@@ -1,17 +1,17 @@
 export interface MapData {
-  address_name: string;
-  category_name: string;
-  distance?: string;
-  id: string;
-  phone?: string;
-  place_name: string;
-  road_address_name: string;
-  x: string;
-  y: string;
+	address_name: string;
+	category_name: string;
+	distance?: string;
+	id: string;
+	phone?: string;
+	place_name: string;
+	road_address_name: string;
+	x: string;
+	y: string;
 }
 
 export interface MapState {
-  data: MapData[];
-  loading: boolean;
-  error: boolean;
+	data: MapData[];
+	loading: boolean;
+	error: boolean;
 }

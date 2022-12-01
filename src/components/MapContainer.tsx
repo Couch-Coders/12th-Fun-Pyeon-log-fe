2657ea521fc35e409b29f023e6b44e0354863f7c
michoball <myunghun0114@gmail.com>
2022-12-01 23:24:40 +0900

    feat : set kakao map arguments type

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
-import { propsType } from './List';
+// import { MapData } from '@stores/map/mapType'
+import React, { useEffect } from 'react'
+// import useScript from 'react-script-hook'
+interface MapPropsType {
+  keyword: string
+}
 
 // head에 작성한 Kakao API 불러오기
-const { kakao } = window as any
+const { kakao } = window
 
-const Map: React.FC<MapPropsType> = ({ keyword }) => {
+const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
   // 검색어가 바뀔 때마다 재렌더링되도록 useEffect 사용
   useEffect(() => {
-    const mapContainer = document.getElementById('map')
-    
+    const mapContainer = document.getElementById('map') as HTMLDivElement
     // 카카오가 undefined인지 체크 코드 필요
     const mapOption = {
       center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
       level: 3, // 지도의 확대 레벨
     }
-
-    // 지도를 생성
-    const map = new kakao.maps.Map(mapContainer, mapOption)
     // 지도를 생성
     const map = new kakao.maps.Map(mapContainer, mapOption)
 
@@ -26,18 +26,15 @@ const Map: React.FC<MapPropsType> = ({ keyword }) => {
       const level = map.getLevel()
 
       // 지도의 중심좌표를 얻어옵니다
-      const latlng = map.getCenter()
-      // 지도의 중심좌표를 얻어옵니다
-      const latlng = map.getCenter()
+      //   const latlng = map.getCenter()
 
-      let message = `<p>지도 레벨은${level}이고</p>`
-      message += `<p>중심 좌표는 위도 ${latlng.getLat()} / 경도 ${latlng.getLng()}입니다</p>`
-      let message = `<p>지도 레벨은${level}이고</p>`
-      message += `<p>중심 좌표는 위도 ${latlng.getLat()} / 경도 ${latlng.getLng()}입니다</p>`
+      const lat = map.getCenter().getLat()
 
-      const resultDiv = document.getElementById('result')!
-      resultDiv.innerHTML = message
-    })
+      const lng = map.getCenter().getLng()
+
+      let message = `<p>지도 레벨은${+level}이고</p>`
+      message += `<p>중심 좌표는 위도 ${+lat} / 경도 ${+lng}입니다</p>`
+
       const resultDiv = document.getElementById('result') as HTMLDivElement
       resultDiv.innerHTML = message
     })
@@ -52,7 +49,10 @@ const Map: React.FC<MapPropsType> = ({ keyword }) => {
     ps.keywordSearch(`${keyword} 편의점`, placesSearchCB)
 
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-    function placesSearchCB(data: any, status: any, pagination: any) {
+    function placesSearchCB(
+      data: kakao.maps.services.PlacesSearchResult,
+      status: kakao.maps.services.Status
+    ) {
       if (status === kakao.maps.services.Status.OK) {
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
@@ -60,8 +60,7 @@ const Map: React.FC<MapPropsType> = ({ keyword }) => {
 
         for (let i = 0; i < data.length; i++) {
           displayMarker(data[i])
-          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
-          console.log(data[i])
+          bounds.extend(new kakao.maps.LatLng(+data[i].y, +data[i].x))
         }
 
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
@@ -72,18 +71,20 @@ const Map: React.FC<MapPropsType> = ({ keyword }) => {
     }
 
     // 지도에 마커를 표시하는 함수입니다
-    function displayMarker(place: any) {
+    function displayMarker(place: kakao.maps.services.PlacesSearchResultItem) {
       // 마커를 생성하고 지도에 표시합니다
       const marker = new kakao.maps.Marker({
         map,
-        position: new kakao.maps.LatLng(place.y, place.x),
+        position: new kakao.maps.LatLng(+place.y, +place.x),
       })
 
       // 마커에 클릭이벤트를 등록합니다
       kakao.maps.event.addListener(marker, 'click', function () {
+        const name = String(place.place_name)
+
         // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
         infowindow.setContent(
-          `<div style="padding:5px;font-size:12px;">${place.place_name}</div>`
+          `<div style="padding:5px;font-size:12px;">${name}</div>`
         )
         infowindow.open(map, marker)
       })
@@ -102,4 +103,4 @@ const Map: React.FC<MapPropsType> = ({ keyword }) => {
   )
 }
 
-export default Map
+export default MapContainer

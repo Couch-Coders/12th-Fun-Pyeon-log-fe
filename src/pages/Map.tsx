2dd06c5ec6d90a4cb4322da2bebe552079ca5924
michoball <myunghun0114@gmail.com>
2022-12-01 23:25:56 +0900

    feat : Apply useScript hook

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,48 +1,60 @@
-import React, { useState } from "react";
-import MapContainer from "@components/MapContainer";
-import { useSelector } from "react-redux";
-import type { RootState } from "@stores/store";
-import { useAppDispatch } from "@stores/store";
-import { getMapThunk } from "@stores/map/mapThunk";
-import List from "@components/List";
+import React, { useState } from 'react'
+import MapContainer from '@components/MapContainer'
+// import { useSelector } from "react-redux";
+// import type { RootState } from "@stores/store";
+import { useAppDispatch } from '@stores/store'
+import { getMapThunk } from '@stores/map/mapThunk'
+// import List from "@components/List";
+
+import useScript from 'react-script-hook/lib/use-script'
 
 const Map = () => {
-  const mapData = useSelector((state: RootState) => state.map.data);
-  const dispatch = useAppDispatch();
+  const [loading, error] = useScript({
+    src: '//dapi.kakao.com/v2/maps/sdk.js?appkey=d25f19cf0a1860dd105275f8a970b86d&libraries=services',
+    onload: () => console.log('Script loaded!'),
+  })
 
-  const [value, setValue] = useState<string>("");
-  const [keyword, setKeyword] = useState("");
+  // const mapData = useSelector((state: RootState) => state.map.data);
+  const dispatch = useAppDispatch()
+
+  const [value, setValue] = useState<string>('')
+  const [keyword, setKeyword] = useState<string>('')
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
+    setValue(e.target.value)
+  }
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!value) {
-      alert("검색어를 입력해주세요.");
+      alert('검색어를 입력해주세요.')
     } else {
-      dispatch(getMapThunk(value));
-      setKeyword(value);
-      setValue("");
+      dispatch(getMapThunk(value))
+      setKeyword(value)
+      setValue('')
     }
-  };
+  }
 
   return (
     <div>
-      <h1>지도 검색</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={value} onChange={onChange} />
-        <input type="submit" value="검색" />
-      </form>
+      {!loading && !error ? (
+        <>
+          <h1>지도 검색</h1>
+          <form onSubmit={onSubmit}>
+            <input type="text" value={value} onChange={onChange} />
+            <input type="submit" value="검색" />
+          </form>
+          <MapContainer keyword={keyword} />
+        </>
+      ) : (
+        <div>error</div>
+      )}
 
-      <MapContainer keyword={keyword} />
-
-      {mapData?.map((map) => (
+      {/* {mapData?.map((map) => (
         <List key={map.id} {...map} />
-      ))}
+      ))} */}
     </div>
-  );
-};
+  )
+}
 
-export default Map;
+export default Map

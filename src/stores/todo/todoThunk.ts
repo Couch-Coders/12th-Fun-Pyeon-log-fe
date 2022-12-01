3b177f97b089b029d17d6a1375bb0164f2075ef5
michoball <myunghun0114@gmail.com>
2022-12-01 23:26:41 +0900

    feat : Change thunk error handling code

--- src/stores/todo/todoThunk.ts
+++ src/stores/todo/todoThunk.ts
@@ -1,21 +1,22 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios, { AxiosError } from "axios";
-import type { Post } from "./todoSlice";
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import axios, { AxiosError } from 'axios'
+import type { Post } from './todoSlice'
 
-const JSON_RANDOMAPI = "https://jsonplaceholder.typicode.com/posts?_limit=10";
+const JSON_RANDOMAPI = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
 
-export const getRandomData = createAsyncThunk( //  fetchRandomData
-  "todo/randomData",
+export const getRandomData = createAsyncThunk(
+  //  fetchRandomData
+  'todo/randomData',
   async (_, thunkApi) => {
     try {
-      const response = await axios.get<Post[]>(JSON_RANDOMAPI);
-      return response.data ;
+      const response = await axios.get<Post[]>(JSON_RANDOMAPI)
+      return response.data
     } catch (error) {
-      if(error instanceof AxiosError){
-        return thunkApi.rejectWithValue(error.message);
-      }else{
+      if (error instanceof AxiosError) {
+        return thunkApi.rejectWithValue(error.message)
+      } else {
         throw error
       }
     }
   }
-);
+)

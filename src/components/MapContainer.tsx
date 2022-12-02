089fd9518fcccaa68bcc241063a37773cb0acabf
michoball <myunghun0114@gmail.com>
2022-12-02 17:11:31 +0900

    fix : fix kakao script load error

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -13,6 +13,7 @@ export interface ResultPropsType {
 }
 // head에 작성한 Kakao API 불러오기
 const { kakao } = window
+
 const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
   // const MapContainer = () => {
   const [mapApi, setMapApi] = useState<kakao.maps.Map | null>(null)
@@ -44,9 +45,7 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
 
         // 지도의 중심좌표를 얻어옵니다
         //   const latlng = map.getCenter()
-
         const lat = mapApi.getCenter().getLat()
-
         const lng = mapApi.getCenter().getLng()
         setMapValue({ level, lat, lng })
       })

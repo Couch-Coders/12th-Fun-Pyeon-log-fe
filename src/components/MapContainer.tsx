92b4211d1a33b755ee3274e60afd7511f785659e
yjkim0109 <yjkkim0109@gmail.com>
2022-12-03 21:15:25 +0900

    feat : Add search Option and moveToCenter function

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -7,28 +7,41 @@ import styled from 'styled-components'
 // import useScript from 'react-script-hook'
 
 const MapCon = styled.div`
+  position: relative;
   width: 100%;
   .map {
     width: 100%;
-    height: calc(100vh - 93px);
+    height: calc(100vh - 80px);
   }
 `
 
-const Button = styled.div`
-  width: 100px;
-  height: 50px;
+const Button = styled.button`
   background-color: #fff;
   color: #222;
   border-radius: 10px;
+  opacity: 0.7;
   position: absolute;
-  top: 100px;
-  right: 10px;
+  top: 10px;
   z-index: 5;
+  transition: 0.5s;
+  padding: 10px;
 
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &.myGps_Btn {
+    right: 10px;
+  }
+
+  &.search_Btn {
+    left: 50%;
+  }
 `
 
 interface MapPropsType {
@@ -40,6 +53,7 @@ export interface ResultPropsType {
   lat: number
   lng: number
 }
+
 // head에 작성한 Kakao API 불러오기
 const { kakao } = window
 
@@ -54,47 +68,23 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
   })
   const mapRef = useRef<HTMLDivElement>(null)
 
-  const toCenter = () => {
-    if (mapApi) {
-      // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
-      if (navigator.geolocation) {
-        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-        navigator.geolocation.getCurrentPosition(function (position) {
-          const lat = position.coords.latitude // 위도
-          const lon = position.coords.longitude // 경도
-
-          const locPosition = new kakao.maps.LatLng(lat, lon) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-          const message = '<div style="padding:5px;">여기에 계신가요?!</div>' // 인포윈도우에 표시될 내용입니다
-
-          displayMe(mapApi, locPosition, message)
-        })
-      } else {
-        // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-
-        const locPosition = new kakao.maps.LatLng(33.450701, 126.570667)
-        const message = 'geolocation을 사용할수 없어요..'
-        displayMe(mapApi, locPosition, message)
-      }
-    }
-  }
-
   useEffect(() => {
-    // 지도 초기화
-    mapRef.current.innerHTML = ''
-
-    const mapContainer = document.getElementById('map') as HTMLDivElement
-    // 카카오가 undefined인지 체크 코드 필요
-    const mapOption = {
-      center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    }
-    // 지도를 생성
-    const map = new kakao.maps.Map(mapContainer, mapOption)
-    setMapApi(map)
+    getMyGps()
   }, [])
 
   // 검색어가 바뀔 때마다 재렌더링되도록 useEffect 사용
   useEffect(() => {
+    search()
+  }, [mapApi, keyword])
+
+  const searchOption = {
+    location: new kakao.maps.LatLng(mapValue.lat, mapValue.lng),
+    radius: 1000,
+    sort: kakao.maps.services.SortBy.DISTANCE,
+  }
+
+  // 검색 함수
+  const search = () => {
     if (mapApi) {
       // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
       kakao.maps.event.addListener(mapApi, 'center_changed', function () {
@@ -106,6 +96,7 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
         const lat = mapApi.getCenter().getLat()
         const lng = mapApi.getCenter().getLng()
         setMapValue({ level, lat, lng })
+        console.log(mapValue.lat, mapValue.lng)
       })
 
       // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
@@ -115,11 +106,13 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
       const ps = new kakao.maps.services.Places()
 
       // 키워드로 장소를 검색합니다
-      ps.keywordSearch(`${keyword} 편의점`, (data, status) =>
-        placesSearchCB(data, status, mapApi, infowindow)
+      ps.keywordSearch(
+        `${keyword} 편의점`,
+        (data, status) => placesSearchCB(data, status, mapApi, infowindow),
+        searchOption
       )
     }
-  }, [mapApi, keyword])
+  }
 
   // 키워드 검색 완료 시 호출되는 콜백함수 입니다
   function placesSearchCB(
@@ -143,7 +136,9 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
 
       // mapData dispatch
       dispatch(getData(data))
+      console.log('검색 완료')
     } else {
+      dispatch(getData([]))
       console.log('error')
     }
   }
@@ -173,7 +168,11 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
   }
 
   // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-  function displayMe(map: kakao.maps.Map, locPosition: any, message: string) {
+  function displayMe(
+    map: kakao.maps.Map,
+    locPosition: kakao.maps.LatLng,
+    message: string
+  ) {
     // 마커를 생성합니다
     const marker = new kakao.maps.Marker({
       map,
@@ -197,6 +196,68 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
     map.setLevel(3, { animate: true })
   }
 
+  // 내 위치로 이동 함수
+  const moveToCenter = () => {
+    if (mapApi) {
+      // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+      if (navigator.geolocation) {
+        // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+        navigator.geolocation.getCurrentPosition(function (position) {
+          const lat = position.coords.latitude // 위도
+          const lng = position.coords.longitude // 경도
+
+          setMapValue({ level: 3, lat, lng })
+
+          const locPosition = new kakao.maps.LatLng(lat, lng) // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+          const message = '<div style="padding:5px;">현 위치</div>' // 인포윈도우에 표시될 내용입니다
+
+          displayMe(mapApi, locPosition, message)
+        })
+      } else {
+        // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+        const locPosition = new kakao.maps.LatLng(mapValue.lat, mapValue.lng)
+        const message = 'geolocation을 사용할수 없어요..'
+
+        displayMe(mapApi, locPosition, message)
+      }
+    }
+  }
+
+  // 내 위치에서 지도 생성
+  const getMyGps = () => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const lat = position.coords.latitude // 위도
+          const lng = position.coords.longitude // 경도
+
+          const center = new kakao.maps.LatLng(lat, lng)
+          setMapValue({ level: 3, lat, lng })
+          drawMap(center)
+        },
+        function () {
+          const center = new kakao.maps.LatLng(mapValue.lat, mapValue.lng)
+          drawMap(center)
+        }
+      )
+    }
+  }
+
+  // 지도 생성 함수
+  const drawMap = (center: kakao.maps.LatLng) => {
+    // 지도 초기화
+    mapRef.current.innerHTML = ''
+
+    const mapContainer = document.getElementById('map') as HTMLDivElement
+    const mapOption = {
+      center, // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    }
+    const map = new kakao.maps.Map(mapContainer, mapOption)
+    setMapApi(map)
+  }
+
   return (
     <MapCon className="map-container">
       <div
@@ -205,7 +266,13 @@ const MapContainer: React.FC<MapPropsType> = ({ keyword }) => {
         style={{ width: '100%', height: '80vh' }}
       ></div>
       <Result level={mapValue.level} lat={mapValue.lat} lng={mapValue.lng} />
-      <Button onClick={toCenter}>내위치로 이동</Button>
+
+      <Button onClick={search} className="search_Btn">
+        이 위치에서 다시 검색
+      </Button>
+      <Button onClick={moveToCenter} className="myGps_Btn">
+        내위치로 이동
+      </Button>
     </MapCon>
   )
 }

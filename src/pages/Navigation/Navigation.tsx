221d35d4501a5c806fb8582d59ccae1dc70ec386
yjkim0109 <yjkkim0109@gmail.com>
2022-12-08 23:07:09 +0900

    feat : Add Navigation Component

---
+++ src/pages/Navigation/Navigation.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { ReactComponent as Funlogo } from '../../assets/fun-pyeon-logo.svg'
+import FunButton from '@styles/FunButton'
+import {
+  NavigationContainer,
+  LogoContainer,
+  Avatar,
+  LogoutContainer,
+} from './Navigation.styles'
+import LoginModal from '@components/LoginModal/LoginModal'
+
+const Navigation = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false)
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  return (
+    <>
+      <NavigationContainer>
+        <LogoContainer>
+          <Funlogo />
+        </LogoContainer>
+        {isLogin ? (
+          <LogoutContainer>
+            <Avatar></Avatar>
+            <p>nickname</p>
+            <FunButton
+              onClick={() => {
+                setIsLogin(false)
+              }}
+            >
+              Logout
+            </FunButton>
+          </LogoutContainer>
+        ) : (
+          <FunButton
+            onClick={() => {
+              setIsLogin(true)
+              setModalOpen(true)
+            }}
+          >
+            Login
+          </FunButton>
+        )}
+        {modalOpen && <LoginModal setModalOpen={setModalOpen} />}
+      </NavigationContainer>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+export default Navigation

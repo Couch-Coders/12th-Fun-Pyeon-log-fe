9837ab9c882b1db30346084f6c0730eda48eb96e
DESKTOP-E2KM8TG\82106 <myunghun0114@gmail.com>
2022-12-08 23:02:14 +0900

    refactor : Change List.tsx Style component name to ListConatiner

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 
-const ConBox = styled.div`
+const ListContainer = styled.div`
   border: 1px solid #222;
   border-radius: 10px;
   padding: 10px;
@@ -14,10 +14,10 @@ interface ListProps {
 const List: FC<ListProps> = ({ placeName }) => {
   return (
     <div>
-      <ConBox>
+      <ListContainer>
         <p>{placeName}</p>
         <p>제품이 다양해요</p>
-      </ConBox>
+      </ListContainer>
     </div>
   )
 }

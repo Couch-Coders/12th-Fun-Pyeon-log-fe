ef5404578cd3fe0845123fc7753aca9e38f2eb99
yjkim0109 <yjkkim0109@gmail.com>
2022-12-08 23:05:23 +0900

    feat : Add Navigation route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-
 import Map from '@pages/Map'
+import Navigation from '@pages/Navigation/Navigation'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Map />} />
+        <Route path="/" element={<Navigation />}>
+          <Route path="/" element={<Map />} />
+        </Route>
       </Routes>
     </div>
   )

6d1531ee0cfa2c13d983f935c270dfc84106054f
yjkim0109 <yjkkim0109@gmail.com>
2022-12-10 00:17:15 +0900

    feat : Apply redux and functions

--- src/pages/Navigation/Navigation.tsx
+++ src/pages/Navigation/Navigation.tsx
@@ -10,12 +10,31 @@ import {
 } from './Navigation.styles'
 import LoginModal from '@components/LoginModal/LoginModal'
 import { googleSignOut } from '@services/firebaseAuth'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@stores/store'
+import { authService } from '@services/authService'
+import { setUser } from '@stores/auth/authSlice'
+import Spinner from '@styles/Spinner'
 
 const Navigation = () => {
   const navigate = useNavigate()
-  const [isLogin, setIsLogin] = useState<boolean>(false)
+  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user.user)
+  const loading = useSelector((state: RootState) => state.user.loading)
   const [modalOpen, setModalOpen] = useState<boolean>(false)
 
+  const signOutHandler = async () => {
+    googleSignOut()
+    if (user) {
+      try {
+        await authService.signOut({ token: user.token })
+        console.log('signOutSuccess')
+        dispatch(setUser(null))
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
   return (
     <>
       <NavigationContainer>
@@ -26,29 +45,25 @@ const Navigation = () => {
             }}
           />
         </LogoContainer>
-        {isLogin ? (
+
+        {loading ? (
+          <Spinner />
+        ) : user ? (
           <LogoutContainer>
             <Avatar></Avatar>
-            <p>nickname</p>
-            <FunButton
-              onClick={() => {
-                setIsLogin(false)
-                googleSignOut()
-              }}
-            >
-              Logout
-            </FunButton>
+            <p>{user.email.split('@')[0]}</p>
+            <FunButton onClick={signOutHandler}>Logout</FunButton>
           </LogoutContainer>
         ) : (
           <FunButton
             onClick={() => {
-              setIsLogin(true)
               setModalOpen(true)
             }}
           >
             Login
           </FunButton>
         )}
+
         {modalOpen && <LoginModal setModalOpen={setModalOpen} />}
       </NavigationContainer>
 

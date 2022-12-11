9d08f09faacbc6fc48f525afd6491bbc3de570c7
yjkim0109 <yjkkim0109@gmail.com>
2022-12-11 21:46:59 +0900

    feat : Add Brand component

---
+++ src/components/Brand/Brand.tsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import { Title } from '@components/FilterBox/FilterBox.styles'
+import { BrandList } from './Brand.styles'
+
+const brands = ['GS25', 'CU', '세븐일레븐', '이마트24', '미니스톱', '기타']
+
+const BrandBox = () => {
+  const brandRef = useRef<HTMLLIElement[] | null[]>([])
+
+  const onClick = (idx: number) => {
+    brandRef.current[idx]?.classList.toggle('on')
+  }
+
+  return (
+    <div>
+      <Title>브랜드</Title>
+      <ul>
+        {brands.map((brand, idx) => (
+          <BrandList
+            ref={(el) => (brandRef.current[idx] = el)}
+            key={idx}
+            onClick={() => onClick(idx)}
+          >
+            {brand}
+          </BrandList>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default BrandBox

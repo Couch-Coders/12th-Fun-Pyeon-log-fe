65a25ce4f0563045a687a469270484970e6e5322
michoball <myunghun0114@gmail.com>
2022-12-12 00:03:04 +0900

    feat : Add basic Review List Style

--- src/components/Review/ReviewList/ReviewList.tsx
+++ src/components/Review/ReviewList/ReviewList.tsx
@@ -1,9 +1,26 @@
+import { PlusOutlined } from '@ant-design/icons'
 import React from 'react'
 
-import { ReviewListContainer } from './ReviewList.styles'
+import { ReviewListWrapper, ReviewTop, NameNCount } from './ReviewList.styles'
 
 const ReviewList = () => {
-  return <ReviewListContainer>REVIEW</ReviewListContainer>
+  return (
+    <ReviewListWrapper>
+      <ReviewTop>
+        <NameNCount>
+          <h1>REVIEW</h1>
+          <div className="count">
+            <PlusOutlined />
+            <p>23</p>
+          </div>
+        </NameNCount>
+        <div className="button">
+          <button>작성하기</button>
+        </div>
+      </ReviewTop>
+      <div className="list-container"></div>
+    </ReviewListWrapper>
+  )
 }
 
 export default ReviewList

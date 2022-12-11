44dfb763053d525791fff6582f41052acb6615e2
michoball <myunghun0114@gmail.com>
2022-12-11 23:31:36 +0900

    feat : Add new Review List component

---
+++ src/components/Review/ReviewList/ReviewList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { ReviewListContainer } from './ReviewList.styles'
+
+const ReviewList = () => {
+  return <ReviewListContainer>REVIEW</ReviewListContainer>
+}
+
+export default ReviewList

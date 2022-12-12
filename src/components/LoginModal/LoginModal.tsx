33c46c65d1af86e5b0c16312f68eb087be624627
yjkim0109 <yjkkim0109@gmail.com>
2022-12-12 20:53:44 +0900

    refactor : Modify props type

--- src/components/LoginModal/LoginModal.tsx
+++ src/components/LoginModal/LoginModal.tsx
@@ -1,5 +1,5 @@
-import { googleSignIn } from '@services/firebaseAuth'
 import React from 'react'
+import { googleSignIn } from '@services/firebaseAuth'
 import {
   LoginWrap,
   LoginBox,
@@ -9,7 +9,7 @@ import {
 } from './LoginModal.styles'
 
 interface ModalProps {
-  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setModalOpen: (modalOpen: boolean) => void
 }
 
 const LoginModal = ({ setModalOpen }: ModalProps) => {

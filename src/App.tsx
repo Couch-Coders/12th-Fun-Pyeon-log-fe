c596dc7e2ced9a28aaed083f65e2186a2fce7776
yjkim0109 <yjkkim0109@gmail.com>
2022-12-12 18:02:01 +0900

    refactor : Arrange import declarations in proper order and Remove unnecessary fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import Map from '@pages/Map'
 import Navigation from '@pages/Navigation/Navigation'
-import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route path="/" element={<Map />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="/" element={<Map />} />
+      </Route>
+    </Routes>
   )
 }
 

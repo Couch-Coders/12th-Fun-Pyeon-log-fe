17b063d5391551d086c971ce5a2f3021524f6192
yjkim0109 <yjkkim0109@gmail.com>
2022-12-13 00:41:49 +0900

    fix: Complete merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-
+import React, { useEffect } from 'react'
 import Main from '@pages/Main'
+import Navigation from '@pages/Navigation/Navigation'
+import { Route, Routes } from 'react-router-dom'
+import { auth } from '@services/firebaseAuth'
+import { onAuthStateChanged } from 'firebase/auth'
+import { getUserThunk } from '@stores/auth/authThunk'
+import { useAppDispatch } from '@stores/store'
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const token: string = await user.getIdToken()
+        dispatch(getUserThunk(token))
+        console.log(user)
+      }
+    })
+    return unsubscribe
+  }, [])
+
   return (
-    <div>
-      <Routes>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
         <Route path="/" element={<Main />} />
-      </Routes>
-    </div>
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/pages/Navigation/Navigation.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { googleSignOut } from '@services/firebaseAuth'
+import { authService } from '@services/authService'
+import { RootState } from '@stores/store'
+import { setUser } from '@stores/auth/authSlice'
+
+import LoginModal from '@components/LoginModal/LoginModal'
+import Spinner from '@styles/Spinner'
+import FunButton, { BUTTON_TYPE_CLASSES } from '@styles/FunButton'
+import { ReactComponent as Funlogo } from '../../assets/fun-pyeon-logo.svg'
+import { NavCon, LogoCon, Avatar, LogoutCon } from './Navigation.styles'
+
+const Navigation = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user.user)
+  const loading = useSelector((state: RootState) => state.user.loading)
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+  const signOutHandler = async () => {
+    googleSignOut()
+    if (user) {
+      try {
+        await authService.signOut({ token: user.token })
+        console.log('signOutSuccess')
+        dispatch(setUser(null))
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+  return (
+    <>
+      <NavCon>
+        <LogoCon>
+          <Funlogo
+            onClick={() => {
+              navigate('/')
+            }}
+          />
+        </LogoCon>
+        
+       {loading ? (
+          <Spinner />
+        ) : user ? (
+          <LogoutCon>
+            <Avatar>
+              <img
+                src="https://lh3.googleusercontent.com/a/AEdFTp4oKPFW_6nqYPabxkYl1wZ8zvdbYIvb7Ndo7nJh=s96-c"
+                alt=""
+              />
+            </Avatar>
+           <p>{user.displayName}</p>
+            <FunButton
+              name={'Logout'}
+              onClick={() => {
+                setIsLogin(false)
+                onClick={signOutHandler}
+              }}
+            />
+          </LogoutCon>
+        ) : (
+          <FunButton
+            name={'Login'}
+            onClick={() => {
+              setModalOpen(true)
+            }}
+          />
+        )}
+
+        {modalOpen && <LoginModal setModalOpen={setModalOpen} />}
+      </NavCon>
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+export default Navigation

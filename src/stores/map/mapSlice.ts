5b2190e67fa2da36e334182873043e3bd6e96181
yjkim0109 <yjkkim0109@gmail.com>
2022-12-13 23:10:21 +0900

    fix : Fix error

--- src/stores/map/mapSlice.ts
+++ src/stores/map/mapSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 // import { getMapThunk } from './mapThunk'
-import { MapData, MapState } from './mapType'
+import { MapState } from './mapType'
 
 const initialState: MapState = {
   data: [],
@@ -12,7 +12,10 @@ const mapSlice = createSlice({
   name: 'map',
   initialState,
   reducers: {
-    getData: (state, action: PayloadAction<MapData[]>) => {
+    getData: (
+      state,
+      action: PayloadAction<kakao.maps.services.PlacesSearchResultItem[]>
+    ) => {
       state.data = action.payload
       // console.log(action.payload)
     },

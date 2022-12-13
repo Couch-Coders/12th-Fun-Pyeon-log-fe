4d149a7303aad93b861464328abd82d66857f35c
michoball <myunghun0114@gmail.com>
2022-12-13 16:15:15 +0900

    feat : Add Review page map wrapper style

--- src/pages/review/Review.styles.ts
+++ src/pages/review/Review.styles.ts
@@ -6,3 +6,14 @@ export const ReviewWrapper = styled.section`
   align-items: center;
   padding: 50px 0;
 `
+export const ReviewMapWrapper = styled.div`
+  margin-top: 20px;
+  width: 80%;
+  min-width: 1000px;
+  height: 500px;
+
+  .map {
+    width: 100%;
+    height: 100%;
+  }
+`

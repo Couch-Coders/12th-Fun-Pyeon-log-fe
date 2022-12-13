9fd856f6a612cdc366a34beec4345d3ee8d653b0
michoball <myunghun0114@gmail.com>
2022-12-13 16:03:01 +0900

    feat : Add Map view in Review page

--- src/pages/review/Review.tsx
+++ src/pages/review/Review.tsx
@@ -1,15 +1,33 @@
-import { ReviewWrapper } from './Review.styles'
-import React from 'react'
+import { ReviewWrapper, ReviewMapWrapper } from './Review.styles'
+import React, { useState, useEffect, useRef } from 'react'
 import ReviewBasicInfo from '@components/Review/ReviewBasicInfo/ReviewBasicInfo'
-import ReviewList from '@components/Review/ReviewList/ReviewList'
+import ReviewList from '@components/Review/ReviewListContainer/ReviewListContainer'
 
 const Review = () => {
+  const mapRef = useRef<HTMLDivElement | null>(null)
+  const [myPosition, setMyPosition] = useState<{ lat: number; lng: number }>({
+    lat: 37.54699,
+    lng: 127.09598,
+  })
+
+  useEffect(() => {
+    if (mapRef.current) mapRef.current.innerHTML = ''
+    const mapContainer = mapRef.current as HTMLDivElement
+
+    const mapOption = {
+      center: new kakao.maps.LatLng(myPosition.lat, myPosition.lng), // 지도의 중심좌표
+      level: 4, // 지도의 확대 레벨
+    }
+    const map = new kakao.maps.Map(mapContainer, mapOption)
+  }, [])
+
   return (
     <ReviewWrapper>
-      Review
       <ReviewBasicInfo />
       <ReviewList />
-      <div id="map" className="map"></div>
+      <ReviewMapWrapper>
+        <div id="map" className="map" ref={mapRef}></div>
+      </ReviewMapWrapper>
     </ReviewWrapper>
   )
 }

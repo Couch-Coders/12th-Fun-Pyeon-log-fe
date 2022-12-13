75bee30e68ade4129a60232da2e46027c93d155b
michoball <myunghun0114@gmail.com>
2022-12-13 16:14:48 +0900

    feat : Finish ReviewList component

--- src/components/Review/ReviewList/ReviewList.tsx
+++ src/components/Review/ReviewList/ReviewList.tsx
@@ -1,25 +1,57 @@
-import { PlusOutlined } from '@ant-design/icons'
-import React from 'react'
+import React, { useState } from 'react'
+import { StarFilled } from '@ant-design/icons'
+import {
+  ListContainer,
+  ListInfo,
+  KeywordBox,
+  ReviewWirter,
+} from './ReviewList.styles'
 
-import { ReviewListWrapper, ReviewTop, NameNCount } from './ReviewList.styles'
+interface ReviewType {
+  reviewContent: string
+  createdDate: number
+  starCount: number
+  keywords: string[]
+}
 
-const ReviewList = () => {
+interface ReviewListProps {
+  review: ReviewType
+}
+
+const ReviewList: React.FC<ReviewListProps> = ({ review }) => {
+  const [isWideView, setIsWideView] = useState<boolean>(false)
+
+  const onWideViewHandler = () => {
+    setIsWideView(!isWideView)
+  }
+  console.log(review.keywords)
+
   return (
-    <ReviewListWrapper>
-      <ReviewTop>
-        <NameNCount>
-          <h1>REVIEW</h1>
-          <div className="count">
-            <PlusOutlined />
-            <p>23</p>
-          </div>
-        </NameNCount>
-        <div className="button">
-          <button>작성하기</button>
+    <ListContainer onClick={onWideViewHandler} isWide={isWideView}>
+      <div className="review">{review.reviewContent}</div>
+      <ListInfo>
+        <div className="star_box">
+          <StarFilled />
+          <span>{review.starCount}</span>
         </div>
-      </ReviewTop>
-      <div className="list-container"></div>
-    </ReviewListWrapper>
+        <KeywordBox>
+          <ul>
+            {review.keywords
+              .filter((_, idx) => idx < 2)
+              .map((keyword, idx) => (
+                <li key={idx}>
+                  <span>{keyword}</span>
+                </li>
+              ))}
+          </ul>
+        </KeywordBox>
+
+        <ReviewWirter>
+          <p className="user">편의점 매니아</p>
+          <p className="day">2022.11.17</p>
+        </ReviewWirter>
+      </ListInfo>
+    </ListContainer>
   )
 }
 

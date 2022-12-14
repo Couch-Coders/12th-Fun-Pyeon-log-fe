c443e56967494c32e42dc7be4364faaefda053cb
michoball <myunghun0114@gmail.com>
2022-12-14 17:09:15 +0900

    feat : Make Map context - set mapApi , markers, and delete markers

---
+++ src/context/MapContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useState } from 'react'
+
+interface MapContextType {
+  mapApi: kakao.maps.Map | null
+  markers: kakao.maps.Marker[]
+  setMapApi: (newMap: kakao.maps.Map) => void
+  setMarkers: (newMarker: kakao.maps.Marker) => void
+  deleteMarkers: () => void
+}
+
+export const MapContext = createContext<MapContextType>({
+  mapApi: null,
+  markers: [],
+  setMapApi: (newMap) => {
+    return null
+  },
+  setMarkers: (newMarker) => {
+    return null
+  },
+  deleteMarkers: () => {
+    return null
+  },
+})
+
+const MapProvider = ({ children }: { children: React.ReactNode }) => {
+  const [map, setMap] = useState<kakao.maps.Map | null>(null)
+  const [newMarkers, setNewMarkers] = useState<kakao.maps.Marker[]>([])
+
+  const setMapApi = (newMap: kakao.maps.Map) => {
+    setMap(newMap)
+  }
+  const setMarkers = (newMarker: kakao.maps.Marker) => {
+    setNewMarkers((prev) => [...prev, newMarker])
+  }
+  const deleteMarkers = () => {
+    newMarkers.forEach((markerInfo) => {
+      markerInfo.setMap(null)
+    })
+    setNewMarkers([])
+  }
+
+  const value = {
+    mapApi: map,
+    markers: newMarkers,
+    setMapApi,
+    setMarkers,
+    deleteMarkers,
+  }
+
+  return <MapContext.Provider value={value}>{children}</MapContext.Provider>
+}
+
+export default MapProvider

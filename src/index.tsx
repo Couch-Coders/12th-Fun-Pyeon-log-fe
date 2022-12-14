6061bfcf926d333082271b1c25d5513912cb429a
michoball <myunghun0114@gmail.com>
2022-12-14 17:07:47 +0900

    feat : Add context Provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,21 @@ import { Provider } from 'react-redux'
 import { store } from '@stores/store'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'theme'
+import MapProvider from '@context/MapContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <App />
-        </ThemeProvider>
-      </Provider>
+      <MapProvider>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <App />
+          </ThemeProvider>
+        </Provider>
+      </MapProvider>
     </BrowserRouter>
   </React.StrictMode>
 )

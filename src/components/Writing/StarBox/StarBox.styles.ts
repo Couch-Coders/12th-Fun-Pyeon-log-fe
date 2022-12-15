d261d243ae9e6e95e78045ad52c6dd4856bb8ba1
yjkim0109 <yjkkim0109@gmail.com>
2022-12-15 18:15:23 +0900

    feat : Add style for StarBox.tsx

---
+++ src/components/Writing/StarBox/StarBox.styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Stars = styled.div`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-right: 50px;
+
+  p {
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+
+  ul {
+    padding: 10px;
+    background-color: #fff;
+    border-radius: 50px;
+    box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.15);
+
+    span {
+      color: ${(props) => props.theme.colors.checkBox};
+      margin: 0 2px;
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+
+      &.clicked {
+        color: ${(props) => props.theme.colors.star};
+      }
+    }
+  }
+`

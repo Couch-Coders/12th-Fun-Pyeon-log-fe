d80ea279bdbab42ffb66602e0935555fff2ada65
michoball <myunghun0114@gmail.com>
2022-12-15 23:59:46 +0900

    feat : Apply conv state to ReviewListContainer and get StoreId using userParams

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,11 +1,22 @@
-import { StoreWrapper, StoreMapWrapper } from './Store.styles'
 import React, { useState, useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchStoreInfo } from '@stores/conv/convSlice'
+import { RootState, useAppDispatch } from '@stores/store'
 import StoreBasicInfo from '@components/StoreDisplay/StoreBasicInfo/StoreBasicInfo'
-import ReviewList from '@components/StoreDisplay/ReviewListContainer/ReviewListContainer'
+import ReviewListContainer from '@components/StoreDisplay/ReviewListContainer/ReviewListContainer'
 import Map from '@components/Map/Map'
 import kakaoServie from '@services/kakaoService'
+import { StoreWrapper, StoreMapWrapper } from './Store.styles'
+import { fetchAllReviews } from '@stores/review/reivewSlice'
+import { useSelector } from 'react-redux'
 
 const Store = () => {
+  const dispatch = useAppDispatch()
+
+  const selectedStore = useSelector(
+    (state: RootState) => state.conv.selectedStore
+  )
+  const { storeId } = useParams<string>()
   const mapRef = useRef<HTMLDivElement | null>(null)
   const [myPosition, setMyPosition] = useState<{ lat: number; lng: number }>({
     lat: 37.54699,
@@ -15,24 +26,44 @@ const Store = () => {
   useEffect(() => {
     if (mapRef.current) mapRef.current.innerHTML = ''
     const mapContainer = mapRef.current as HTMLDivElement
-
-    const mapOption = {
-      center: new kakao.maps.LatLng(myPosition.lat, myPosition.lng), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
+    if (selectedStore) {
+      if (!selectedStore.y) {
+        const mapOption = {
+          center: new kakao.maps.LatLng(myPosition.lat, myPosition.lng), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        }
+        const map = new kakao.maps.Map(mapContainer, mapOption)
+        // 임의의 편의점 위치에 마커 생성
+        kakaoServie.displayMyLocation(
+          map,
+          new kakao.maps.LatLng(myPosition.lat, myPosition.lng)
+        )
+      } else {
+        const mapOption = {
+          center: new kakao.maps.LatLng(+selectedStore.y, +selectedStore.x), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        }
+        const map = new kakao.maps.Map(mapContainer, mapOption)
+        // 임의의 편의점 위치에 마커 생성
+        kakaoServie.displayMyLocation(
+          map,
+          new kakao.maps.LatLng(+selectedStore.y, +selectedStore.x)
+        )
+      }
     }
+  }, [selectedStore, myPosition.lat, myPosition.lng])
 
-    const map = new kakao.maps.Map(mapContainer, mapOption)
-    // 임의의 편의점 위치에 마커 생성
-    kakaoServie.displayMyLocation(
-      map,
-      new kakao.maps.LatLng(myPosition.lat, myPosition.lng)
-    )
-  }, [])
+  useEffect(() => {
+    if (storeId) {
+      dispatch(fetchStoreInfo(storeId))
+      dispatch(fetchAllReviews(storeId))
+    }
+  }, [storeId, dispatch])
 
   return (
     <StoreWrapper>
       <StoreBasicInfo />
-      <ReviewList />
+      <ReviewListContainer />
       <StoreMapWrapper>
         <Map ref={mapRef} />
       </StoreMapWrapper>

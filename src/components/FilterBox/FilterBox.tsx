7488a07d5f9526b18a02c8fdfb173409a0110413
yjkim0109 <yjkkim0109@gmail.com>
2022-12-15 16:47:31 +0900

    refactor : Add Select.tsx and useState

--- src/components/FilterBox/FilterBox.tsx
+++ src/components/FilterBox/FilterBox.tsx
@@ -8,13 +8,69 @@ import { BRANDS, FACILITIES, MOOD, PRODUCT } from '@utils/constants'
 import { FilterWrapper, KeywordGroup, Title } from './FilterBox.styles'
 
 const Filter = () => {
+  const [selectBrand, setSelectBrand] = useState<string[]>([])
+  const [selectKeyword, setSelectKeyword] = useState<string[]>([])
+
+  /*
+  const mapData = useSelector((state: RootState) => state.map.data)
+  const dispatch = useDispatch()
+
+  const sortStore = () => {
+    console.log(selectBrand)
+    console.log(selectKeyword)
+
+    const isIncluded = (data: kakao.maps.services.PlacesSearchResultItem) => {
+      for (let idx = 0; idx < selectBrand.length; idx++) {
+        if (data.place_name.includes(selectBrand[idx])) return true
+      }
+    }
+    const newData = mapData.filter(isIncluded)
+    console.log(newData)
+    dispatch(sortData(newData))
+  }
+  */
+
   return (
     <FilterWrapper>
-      <Brand />
-      <KeywordBox />
+      <div>
+        <Title>브랜드</Title>
+        <Select
+          keyword={BRANDS}
+          selected={selectBrand}
+          setSelected={setSelectBrand}
+          selectType={'brand'}
+        />
+      </div>
+
+      <KeywordGroup>
+        <Title>키워드</Title>
+        <Select
+          title="제품"
+          keyword={PRODUCT}
+          selected={selectKeyword}
+          setSelected={setSelectKeyword}
+          selectType={'checkbox'}
+        />
+        <Select
+          title="분위기"
+          keyword={MOOD}
+          selected={selectKeyword}
+          setSelected={setSelectKeyword}
+          selectType={'checkbox'}
+        />
+        <Select
+          title="편의시설"
+          keyword={FACILITIES}
+          selected={selectKeyword}
+          setSelected={setSelectKeyword}
+          selectType={'checkbox'}
+        />
+      </KeywordGroup>
+
       <FunButton
         name={'찾아보기'}
         style={{ width: '100%', minHeight: '30px', fontWeight: '700' }}
+        // onClick={() => sortStore()}
       />
     </FilterWrapper>
   )

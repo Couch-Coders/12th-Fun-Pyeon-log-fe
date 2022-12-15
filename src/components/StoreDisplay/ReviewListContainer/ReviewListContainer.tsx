2438aaea9c8b675a05b538731f12dfb67432e037
michoball <myunghun0114@gmail.com>
2022-12-16 00:00:37 +0900

    refactor : Change review dummy data value and import review state value

--- src/components/StoreDisplay/ReviewListContainer/ReviewListContainer.tsx
+++ src/components/StoreDisplay/ReviewListContainer/ReviewListContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReviewList from '@components/StoreDisplay/ReviewList/ReviewList'
 import { PlusOutlined } from '@ant-design/icons'
 import FunButton, { BUTTON_TYPE_CLASSES } from '@styles/FunButton'
@@ -8,6 +8,9 @@ import {
   NameNCount,
   ListContainer,
 } from './ReviewListContainer.styles'
+import { useSelector } from 'react-redux'
+import { RootState } from '@stores/store'
+import { ReviewType } from '@stores/review/reviewType'
 
 const reviewData = [
   {
@@ -15,9 +18,9 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱... 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.0,
-    createdDate: '2022. 12. 25',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 4,
+    user: ' 4',
     keywords: [
       '트렌디한 상품이 많아요',
       '분위기가 좋아요',
@@ -31,9 +34,9 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.5,
-    createdDate: '2022. 12. 22',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 1,
+    user: ' 1',
     keywords: ['분위기가 좋아요', '매장이 넓어요'],
   },
   {
@@ -41,9 +44,9 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.7,
-    createdDate: '2022. 12. 20',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 6,
+    user: ' 6',
     keywords: ['분위기가 좋아요', '트렌디한 상품이 많아요', '굿이에요'],
   },
   {
@@ -51,9 +54,9 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱... 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.0,
-    createdDate: '2022. 12. 25',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 7,
+    user: ' 7',
     keywords: [
       '트렌디한 상품이 많아요',
       '분위기가 좋아요',
@@ -67,9 +70,9 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.5,
-    createdDate: '2022. 12. 22',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 8,
+    user: ' 8',
     keywords: ['분위기가 좋아요', '매장이 넓어요'],
   },
   {
@@ -77,15 +80,25 @@ const reviewData = [
     reviewContent:
       '자주 가는 편의점인데 너무 좋아요~ 신상도 많고 버터맥주도 있어요 짱짱...',
     starCount: 4.7,
-    createdDate: '2022. 12. 20',
+    createdDate: new Date(2022, 12, 22),
     storeId: 1,
-    userId: 31,
+    user: '31',
     keywords: ['분위기가 좋아요', '트렌디한 상품이 많아요', '굿이에요'],
   },
 ]
 
 const ReviewListContainer = () => {
-  const [reviews, setReviews] = useState(reviewData)
+  const [reviewList, setReviewList] = useState(reviewData)
+
+  // 서버 통신이 되면 바뀔 부분
+  // const [reviewList, setReviewList] = useState<ReviewType[]>([])
+  // const reviews = useSelector((state: RootState) => state.review.reviews)
+
+  // useEffect(() => {
+  //   if (reviews.length) {
+  //     setReviewList(reviews)
+  //   }
+  // }, [reviews])
 
   return (
     <ReviewListWrapper>
@@ -94,7 +107,7 @@ const ReviewListContainer = () => {
           <h1>REVIEW</h1>
           <div className="count">
             <PlusOutlined />
-            <p>{reviews.length}</p>
+            <p>{reviewList.length}</p>
           </div>
         </NameNCount>
         <div className="button">
@@ -102,14 +115,14 @@ const ReviewListContainer = () => {
         </div>
       </ReviewTop>
       <ListContainer>
-        {reviews.map((review) => (
+        {reviewList.map((review) => (
           <ReviewList
             key={review.reviewId}
             starCount={review.starCount}
             createdDate={review.createdDate}
             keywords={review.keywords}
             reviewContent={review.reviewContent}
-            userId={review.userId}
+            userId={review.user}
           />
         ))}
       </ListContainer>

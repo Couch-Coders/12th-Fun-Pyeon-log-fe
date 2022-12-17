326c6f3a2fd42a79066865b80e669aacea0e5b75
yjkim0109 <yjkkim0109@gmail.com>
2022-12-17 22:20:03 +0900

    feat : Add useSelector for user and Add useNavigate

--- src/pages/Write/Write.tsx
+++ src/pages/Write/Write.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import StoreBasicInfo from '@components/StoreDisplay/StoreBasicInfo/StoreBasicInfo'
 import WritingBox from '@components/Writing/WritingBox/WritingBox'
 import { StoreWrapper } from '@pages/store/Store.styles'
+import { useSelector } from 'react-redux'
+import { RootState } from '@stores/store'
+import { useNavigate } from 'react-router-dom'
 
 const Write = () => {
+  const user = useSelector((state: RootState) => state.user.user)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) {
+      alert('잘못된 접근입니다.')
+      navigate('/', { replace: true })
+    }
+  }, [])
+
   return (
     <StoreWrapper>
       <StoreBasicInfo />

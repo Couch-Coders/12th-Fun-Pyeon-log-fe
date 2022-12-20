b0735ea04f76ae412aca249d98f4d866fc39b452
michoball <myunghun0114@gmail.com>
2022-12-20 17:38:20 +0900

    feat : Apply redux-persist

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,21 +1,55 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
+import {
+  persistStore,
+  persistReducer,
+  PersistConfig,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import mapReducer from './map/mapSlice'
 import userReducer from './auth/authSlice'
 import reviewReducer from './review/reivewSlice'
 import convReducer from './conv/convSlice'
 
-export const store = configureStore({
-  reducer: {
-    map: mapReducer,
-    user: userReducer,
-    review: reviewReducer,
-    conv: convReducer,
-  },
+const rootReducer = combineReducers({
+  map: mapReducer,
+  user: userReducer,
+  review: reviewReducer,
+  conv: convReducer,
 })
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: Array<keyof RootState>
+}
+
+const persistConfig: ExtendedPersistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['conv'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
 export const useAppDispatch: () => AppDispatch = useDispatch
+export const persistor = persistStore(store)
 
 export default store

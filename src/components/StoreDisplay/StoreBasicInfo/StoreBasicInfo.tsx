f2810ac7b6b3b1208f971397d990a9827006dd48
michoball <myunghun0114@gmail.com>
2022-12-20 17:44:52 +0900

    feat : Apply keywordbadge component and apply real store value to jsx

--- src/components/StoreDisplay/StoreBasicInfo/StoreBasicInfo.tsx
+++ src/components/StoreDisplay/StoreBasicInfo/StoreBasicInfo.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import GsImg from '../../../assets/convImg/gs.png'
+import React, { useLayoutEffect, useRef, useState } from 'react'
+import funlogImg from '../../../assets/convImg/funlog.png'
 import { PhoneFilled, PushpinFilled, StarFilled } from '@ant-design/icons'
 import {
   ConvImgWrapper,
@@ -8,48 +8,76 @@ import {
   KeywordBox,
   StarPoint,
 } from './StoreBasicInfo.styles'
+import { getBrandImg } from '@services/markerImg'
+import { useSelector } from 'react-redux'
+import { RootState } from '@stores/store'
+import KeywordBadge from '@styles/KeywordBadge'
+
+interface StoreInfo {
+  storeImg: string
+  place_name: string
+  address_name: string
+  phone: string
+  keywordList: string[]
+  starCount: number
+}
 
 const StoreBasicInfo = () => {
+  const imgRef = useRef<HTMLImageElement | null>(null)
+  const selectedStore = useSelector(
+    (state: RootState) => state.conv.selectedStore
+  )
+  const [storeInfo, setStoreInfo] = useState<StoreInfo>({
+    storeImg: funlogImg,
+    place_name: 'FUN편로그 편의점',
+    phone: '  02-525-2525',
+    address_name: ' 서울시 어쩌구 무슨무슨로 2-1',
+    starCount: 3,
+    keywordList: ['제품이 다양해요', '매장이 청결해요', '펀편로그 좋아요'],
+  })
+
+  useLayoutEffect(() => {
+    if (selectedStore) {
+      const [placeName] = selectedStore.place_name.split(' ')
+      setStoreInfo((prevState) => {
+        return {
+          prevState,
+          ...selectedStore,
+          storeImg: getBrandImg(placeName) ?? funlogImg,
+        }
+      })
+    }
+  }, [selectedStore])
+
   return (
     <InfoContainer>
       <ConvImgWrapper>
-        <img src={GsImg} />
+        <img src={storeInfo.storeImg} ref={imgRef} />
       </ConvImgWrapper>
 
       <ConvInfo>
-        <h1>GS25 땡땡점</h1>
+        <h1>{storeInfo.place_name}</h1>
         <p>
           <span>
-            <PushpinFilled /> 서울시 어쩌구 무슨무슨로 2-1
+            <PushpinFilled />
+            {storeInfo.address_name}
           </span>
           <span>
             <PhoneFilled />
-            02-525-2525
+            {storeInfo.phone}
           </span>
         </p>
         <KeywordBox>
           <ul>
-            <li>
-              <span>제품이 다양해요</span>
-            </li>
-            <li>
-              <span>제품이 다양해요</span>
-            </li>
-            <li>
-              <span>제품이 다양해요</span>
-            </li>
-            <li>
-              <span>제품이 다양해요</span>
-            </li>
-            <li>
-              <span>제품이 다양해요</span>
-            </li>
+            {storeInfo.keywordList.slice(0, 5).map((keyword) => {
+              return <KeywordBadge key={keyword}>{keyword}</KeywordBadge>
+            })}
           </ul>
         </KeywordBox>
       </ConvInfo>
       <StarPoint>
         <StarFilled />
-        <p>4.6 / 5</p>
+        <p>{storeInfo.starCount}/ 5</p>
       </StarPoint>
     </InfoContainer>
   )

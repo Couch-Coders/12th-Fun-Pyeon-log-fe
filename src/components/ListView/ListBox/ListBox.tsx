b3c0b08dfe175a4e287d46505280b4f4ab68a0c9
michoball <myunghun0114@gmail.com>
2022-12-21 22:15:03 +0900

    feat : Add mapContext and kakao displayMarkerOverlay funtion

--- src/components/ListView/ListBox/ListBox.tsx
+++ src/components/ListView/ListBox/ListBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import List from '@components/ListView/List/List'
 import { useDispatch, useSelector } from 'react-redux'
 import type { RootState } from '@stores/store'
@@ -7,10 +7,13 @@ import { ConvType } from '@stores/conv/convType'
 import { LIST_SORT_ITEMS } from '@utils/constants'
 import { distanceSort, reviewSort, starSort } from '@stores/conv/convSlice'
 import { saveSortType } from '@stores/sort/sortSlice'
+import KakaoService from '@services/kakaoService'
+import { MapContext } from '@context/MapContext'
 
 const ListBox = () => {
   const sortedConv = useSelector((state: RootState) => state.conv.sortedStores)
   const sortType = useSelector((state: RootState) => state.sort.sortType)
+  const { mapApi, setMarkers } = useContext(MapContext)
   const [convList, setConvList] = useState<ConvType[]>([])
   const dispatch = useDispatch()
   const [select, setSelect] = useState(LIST_SORT_ITEMS[0].type)
@@ -26,9 +29,20 @@ const ListBox = () => {
   useEffect(() => {
     setConvList(sortedConv)
 
-    if (convList.length !== 0) {
+    if (convList.length > 0) {
       toggleBtn(sortType)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortedConv])
+
+  useEffect(() => {
+    if (sortedConv.length > 0 && mapApi) {
+      sortedConv.forEach((list) => {
+        const marker = KakaoService.displayMarkerOverlay(list, mapApi)
+        setMarkers(marker)
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sortedConv])
 
   return (
@@ -59,6 +73,8 @@ const ListBox = () => {
               lat={+store.y}
               lng={+store.x}
               storeId={store.id}
+              address={store.address_name}
+              phoneNumber={store.phone}
             />
           ))
         )}

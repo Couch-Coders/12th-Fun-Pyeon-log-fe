4f992eb425d94fa05adf84c43dde2fb85580355c
yjkim0109 <yjkkim0109@gmail.com>
2022-12-28 22:15:41 +0900

    refactor: Add button type for map

--- src/components/styles/FunButton.tsx
+++ src/components/styles/FunButton.tsx
@@ -1,29 +1,30 @@
 import React, { ButtonHTMLAttributes } from 'react'
-import { BaseBtn, GoogleSignInBtn } from './FunButton.styles'
+import { mapBtn, BaseBtn, GoogleSignInBtn } from './FunButton.styles'
 
 export enum BUTTON_TYPE_CLASSES {
   base = 'base',
   google = 'google',
+  map = 'map',
 }
 
 const getButton = (buttonType = BUTTON_TYPE_CLASSES.base): typeof BaseBtn =>
   ({
     [BUTTON_TYPE_CLASSES.base]: BaseBtn,
     [BUTTON_TYPE_CLASSES.google]: GoogleSignInBtn,
+    [BUTTON_TYPE_CLASSES.map]: mapBtn,
   }[buttonType])
 
 type FunButtonProps = {
-  name: string
   buttonType?: BUTTON_TYPE_CLASSES
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
 const FunButton: React.FC<FunButtonProps> = ({
   buttonType,
-  name,
+  children,
   ...otherProps
 }) => {
   const CustomButton = getButton(buttonType)
-  return <CustomButton {...otherProps}>{name}</CustomButton>
+  return <CustomButton {...otherProps}>{children}</CustomButton>
 }
 
 export default FunButton

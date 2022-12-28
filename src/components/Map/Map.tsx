c1fe5cd69b2619356c50fb564d15b8c13945bf7f
michoball <myunghun0114@gmail.com>
2022-12-28 17:56:55 +0900

    refactor : Change map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,35 @@
-import React, { forwardRef, ReactNode } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { MapContext } from '@context/MapContext'
+import { DEFAULT_KAKAO_COORD } from '@utils/constants'
 import { MapViewer } from './Map.styles'
 
-interface Props {
-  children?: ReactNode
-}
+// interface Props {
+//   children?: ReactNode
+// }
 
-const Map = forwardRef<HTMLDivElement, Props>(function Map(props, ref) {
-  return <MapViewer className="map" ref={ref}></MapViewer>
-})
+// const Map = forwardRef<HTMLDivElement, Props>(function Map(props, ref) {
+//   return <MapViewer className="map" ref={ref}></MapViewer>
+// })
+
+const Map = () => {
+  const mapRef = useRef<HTMLDivElement | null>(null)
+  const { setMapApi } = useContext(MapContext)
+
+  useEffect(() => {
+    if (mapRef.current) mapRef.current.innerHTML = ''
+    const mapContainer = mapRef.current as HTMLDivElement
+    const mapOption = {
+      center: new kakao.maps.LatLng(
+        DEFAULT_KAKAO_COORD.lat,
+        DEFAULT_KAKAO_COORD.lng
+      ), // 지도의 중심좌표
+      level: 4, // 지도의 확대 레벨
+    }
+    const map = new kakao.maps.Map(mapContainer, mapOption)
+    setMapApi(map)
+  }, [setMapApi])
+
+  return <MapViewer className="map" ref={mapRef}></MapViewer>
+}
 
 export default Map

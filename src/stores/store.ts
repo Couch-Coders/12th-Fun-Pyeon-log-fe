5848723e560cf43eec7cd61b8880f57384001b66
michoball <myunghun0114@gmail.com>
2023-01-04 18:33:18 +0900

    refactor : Delete user state in persist whitelist

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -34,7 +34,7 @@ type ExtendedPersistConfig = PersistConfig<RootState> & {
 const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
-  whitelist: ['sort', 'user'],
+  whitelist: ['sort'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)

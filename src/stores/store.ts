da3131fa264270f7c5939556151037e88c48baf2
michoball <myunghun0114@gmail.com>
2023-01-14 23:25:02 +0900

    refactor: Add persist whitelist

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -34,7 +34,7 @@ type ExtendedPersistConfig = PersistConfig<RootState> & {
 const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
-  whitelist: ['sort'],
+  whitelist: ['sort', 'review', 'user', 'conv'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)

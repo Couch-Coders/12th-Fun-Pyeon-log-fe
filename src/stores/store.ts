5ac600edc40159997a37c108a0cfac4ae636ba2f
michoball <myunghun0114@gmail.com>
2023-01-17 00:22:58 +0900

    fix : Add conv and review store to persist whitelist to fix bug

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -34,7 +34,7 @@ type ExtendedPersistConfig = PersistConfig<RootState> & {
 const persistConfig: ExtendedPersistConfig = {
   key: 'root',
   storage,
-  whitelist: ['sort', 'user'],
+  whitelist: ['sort', 'user', 'conv', 'review'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
